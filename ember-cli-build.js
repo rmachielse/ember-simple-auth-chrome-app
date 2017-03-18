bdfb21cb203c2c83a4a6ca2f4a4f7013000c312f
Richard Machielse <richardmachielse@gmail.com>
2017-03-18 16:49:56 +0100

    Upgrading dependencies

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,24 +1,9 @@
-/*jshint node:true*/
-/* global require, module */
-var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+/* eslint-env node */
+const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
-    jscsOptions: {
-      enabled: true,
-      testGenerator: function(relativePath, errors) {
-        if (errors) {
-          errors = "\\n" + this.escapeErrorString(errors);
-        } else {
-          errors = "";
-        }
-
-        return "describe('JSCS - " + relativePath + "', function() {\n" +
-          "it('should pass jscs', function() { \n" +
-          "  expect(" + !errors + ", '" + relativePath + " should pass jscs." + errors + "').to.be.ok; \n" +
-          "})});\n";
-      }
-    }
+    // Add options here
   });
 
   /*
